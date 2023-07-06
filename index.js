941985334d901e89905107bb44d92d5e15d4b98e
JoasMartins <joasmcarmo@gmail.com>
2023-07-06 16:57:48 -0300

    Atualizacao na API: Sistema de Usuários | Operacao: 20

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose")
 const appData = require("./appData.json")
 
 api.use(bodyParser.json());
-app.use(express.json());
+api.use(express.json());
 
 mongoose.connect(appData.api.databaseURL)
     .then(() => {
