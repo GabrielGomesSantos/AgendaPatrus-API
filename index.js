599bb02b093456aa858cf18c4a6f67b425922cde
JoasMartins <joasmcarmo@gmail.com>
2023-07-20 14:18:46 -0300

    Atualizacao na API: Solicitar Tarefas | Operacao: 4

--- index.js
+++ index.js
@@ -230,6 +230,7 @@ api.get("/tasks/one", async (req, res) => {
 
 api.get("/tasks/several", async (req, res) => {
     let contentFind = req.body
+    console.log(req.body)
     console.log(req.query)
 
     if(contentFind.title) {
