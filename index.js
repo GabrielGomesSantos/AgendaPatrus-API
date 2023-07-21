7a8327cb6d42de02d9a3bfaa33801d779499bf6d
JoasMartins <joasmcarmo@gmail.com>
2023-07-21 16:45:48 -0300

    Atualizacao na API: Add ID as tasks | Operacao: 5

--- index.js
+++ index.js
@@ -495,10 +495,10 @@ api.put("/markedtasks", async (req, res) => {
     let allTasks = await modelTask.find()
     console.log(allTasks)
     let contagem = 0
-    allTasks.map((task) => {
+    allTasks.map(async (task) => {
         console.log(task._id)
         contagem++
-        modelTask.findOneAndUpdate({ _id: task._id }, { $set: { id: contagem }}).save()
+        await modelTask.findOneAndUpdate({ _id: task._id }, { $set: { id: contagem }}).save()
             .then((data) => { res.status(200).json({ result: "Sucess", item: task.title, newId: contagem }) })
             .catch((err) => { res.status(400).json(err) })
     })
